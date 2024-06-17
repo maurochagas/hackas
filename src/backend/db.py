6ccc0341bacb968a1b9e1023b4d5377869d9e95a
maurochagas <mauro.jr.1074@gmail.com>
2024-06-16 23:52:34 -0300

    🗃️ add: framework for storing non-relational files

--- src/backend/db.py
+++ src/backend/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData # type: ignore
 from databases import Database # type: ignore
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./database/relational/database.db"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
